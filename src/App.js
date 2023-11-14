49c55e592c20e6534b1bb25604fc0e2a72ee3cf6
adambelguith <adem.belguith@isimg.tn>
2023-11-14 09:43:18 +0100

    fix same style form 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/nav';
 import FormUser from './components/form';
-
+import React,{useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [formData, setFormData] = useState({});
+  const [showFormTwo, setShowFormTwo] = useState(false);
+
+  const handleFormOneSubmit = (data) => {
+    setFormData(data);
+    setShowFormTwo(true);
+  };
+
+  const handleFormTwoSubmit = async (data) => {
+    // Combine data from both forms
+    const combinedData = { ...formData, ...data };
+
+    // Make a POST request using Axios
+    try {
+      const response = await axios.post('your-api-endpoint', combinedData);
+      console.log('Server response:', response.data);
+    } catch (error) {
+      console.error('Error submitting data:', error);
+    }
+  };
+
+
   return (
-    <div className="App h-screen">
+    <div className="App pb-32">
       <NavBar />
       <FormUser />
     </div>
--- src/components/formtwo.jsx
+++ src/components/formtwo.jsx
@@ -3,15 +3,12 @@ import { useForm } from "react-hook-form"
 import "react-datepicker/dist/react-datepicker.css";
 import '../App.css'
 
-
   
 const FormUser = ({ onSubmit }) => {
-
     const { register, handleSubmit, formState: { errors }, } = useForm()
   return (
     <div className='flex  justify-center  '>
       <div className='flex flex-col w-8/12'>
-
         <div className='flex flex-row justify-center mb-8'>
           <div className='etapesdiv h-24 w-24 relative '> <h1 className='mt-8 ml-4 text-white text-xl'>etape1</h1></div>
           <div className='etapesdivnul h-24 w-24 relative'><h1 className='mt-8 ml-4 text-[#151516c4] text-xl'>etape 2</h1></div>
@@ -25,42 +22,23 @@ const FormUser = ({ onSubmit }) => {
                     <div className="text-[#f30808]">entrer votre Prenom obligatoire</div>
                   )}
                   <label className='text-black flex justify-start ml-4 '>Nom :</label>
-                  <input {...register("lastName",{required:'entrer votre Nom obligatoire'})} />
+                  <input {...register("lastName")} />
                   {errors.lastName && (
                     <div className="text-[#f30808]">entrer votre nom obligatoire</div>
                   )}
                   <label className='text-black flex justify-start ml-4 '>Date de naissance  :
-   
                   <div className='mb-6 w-2/5'>
-                  <input type='date' {...register("dateOfBirth",{required:'entrer votre Date de naissance obligatoire'})} />
+                  <input type='date' {...register("dateOfBirth")} />
                   {errors.dateOfBirth && (
-                    <div className="text-[#f30808] whitespace-nowrap">entrer votre Date de naissance obligatoire</div>
+                    <div className="text-[#f30808]">entrer votre Prenom obligatoire</div>
                   )}
                   </div>
                   </label>
-                  {/* <input type="number" {...register("age")} /> */}
-
-                  <label className='text-black flex justify-start ml-4 '>Sexe :</label>
-                  <select
-                  className="w-2/5 h-6 rounded-md"
-                  // id="isPaid"
-                  // defaultValue={order.isPaid} 
-                  {...register('gender', {
-                    required: 'entrer votre sexe obligatoire',
-                  })}
-                  >
-                    <option className='classstyle' ><span className='text-[#7f7575] text-sm'>Votre sexe</span></option>
-                  <option value="M">M</option>
-                  <option value="F">F</option>
-                </select>
-                {errors.gender && (
-                  <div className="text-[#f30808]">entrer votre sexe obligatoire</div>
-                )}
-                
-
+                  <input type="number" {...register("age")} />
                   <button type="submit" > Envoyer </button>
               </div>
           </form>
+         
         
       </div>
     </div>
